0f3ee259ee5fc6ad28a2cbbaae95965fc62b3581
dahuin-1 <dawhite01@naver.com>
2022-06-22 11:47:06 +0900

    add flag too; l

--- event/block_event_listener.go
+++ event/block_event_listener.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/cc-ping-listener/unmarshalers"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/event"
 	mspclient "github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
@@ -19,10 +20,9 @@ type User struct {
 }
 
 const (
-	chainCodeID = "ping"
-	channelID   = "kiesnet-dev"
-	configPath  = "/Users/dhkim/Projects/cc-ping-listener/config/network.yaml"
-	credPath    = "/Users/dhkim/Projects/kiesnet-chaincode-dev-network/crypto-config/peerOrganizations/kiesnet.dev/users"
+	channelID  = "kiesnet-dev"
+	configPath = "/Users/dhkim/Projects/cc-ping-listener/config/network.yaml"
+	credPath   = "/Users/dhkim/Projects/kiesnet-chaincode-dev-network/crypto-config/peerOrganizations/kiesnet.dev/users"
 )
 
 func getChannelProvider() (context.ChannelProvider, error) {
@@ -67,7 +67,18 @@ func setUser() (*User, error) {
 	return &User{Cert: cert, PrivateKey: key}, nil
 }
 
+func getStartBlock(startBlock string) error {
+	log.Printf("hello world, %s", startBlock)
+	return nil
+}
+
 func main() {
+	startBlock := flag.String("startBlock", "", "keep start block number if needed")
+	flag.Parse()
+	err := getStartBlock(*startBlock)
+	if err != nil {
+		log.Fatalf("err")
+	}
 	channelProvider, err := getChannelProvider()
 	if err != nil {
 		log.Fatalf("failed to get Channel Provider, err: %s", err)
@@ -77,52 +88,49 @@ func main() {
 		log.Fatalf("failed to return Client instance, err: %s", err)
 	}
 	registration, eventChannel, err := client.RegisterBlockEvent()
-	//registration, eventChannel, err := client.RegisterChaincodeEvent(chainCodeID, "fruit/soldout|fruit/restock")
 	if err != nil {
 		log.Fatalf("failed to register Block Event, err: %s", err)
 	}
 	defer client.Unregister(registration)
 	for {
 		log.Printf("🎹👂🎹👂🎹👂🎹👂🎹👂🏻listen👂🏻🎹👂🎹👂🎹👂🎹👂🎹")
-		select {
-		case e := <-eventChannel:
-			blockData := e.Block.Data.Data
-			envelope, err := unmarshalers.GetEnvelopeFromBlock(blockData[0])
-			if err != nil {
-				log.Fatalf("unmarshaling Envelope error: %s", err)
-			}
-			payload, err := unmarshalers.GetPayloadFromEnv(envelope.Payload)
-			if err != nil {
-				log.Fatalf("unmarshaling envelopePayload to payload error: %s", err)
-			}
-			transaction, err := unmarshalers.GetTransaction(payload.Data)
-			if err != nil {
-				log.Fatalf("unmarshaling payloadData to transaction error: %s", err)
-			}
-			chaincodeActionPayload, err := unmarshalers.GetChaincodeActionPayload(transaction.Actions[0].Payload)
-			if err != nil {
-				log.Fatalf("unmarshaling transactionActionPayload to chaincodeActionPayload error: %s", err)
-			}
-			proposalResponsePayload, err := unmarshalers.GetProposalResponsePayload(chaincodeActionPayload.Action.ProposalResponsePayload)
-			if err != nil {
-				log.Fatalf("unmarshaling chaincodeActionPayload.Action ProposalResponsePayload to proposalResponsePayload error: %s", err)
-			}
-			chaincodeAction, err := unmarshalers.GetChaincodeAction(proposalResponsePayload.Extension)
-			if err != nil {
-				log.Fatalf("unmarshaling proposalResponsePayload Extension to chaincodeAction error: %s", err)
-			}
-			chaincodeEvent, err := unmarshalers.GetChaincodeEvent(chaincodeAction.Events)
-			if err != nil {
-				log.Fatalf("unmarshaling chaincodeAction.Events to chaincodeEvent error: %s", err)
-			}
-			if chaincodeEvent.EventName != "" {
-				log.Println("############################################################")
-				log.Println("###################### Received event ######################")
-				log.Printf("################### BlockNum : %d ##########################", e.Block.Header.Number)
-				log.Printf("#################### Block event : %v ########### ", chaincodeEvent.EventName)
-				log.Printf("#################### Block info : %v ########### ", chaincodeAction.String())
-				log.Println("#############################################################")
-			}
+		e := <-eventChannel
+		blockData := e.Block.Data.Data
+		envelope, err := unmarshalers.GetEnvelopeFromBlock(blockData[0])
+		if err != nil {
+			log.Fatalf("unmarshaling Envelope error: %s", err)
+		}
+		payload, err := unmarshalers.GetPayloadFromEnv(envelope.Payload)
+		if err != nil {
+			log.Fatalf("unmarshaling envelopePayload to payload error: %s", err)
+		}
+		transaction, err := unmarshalers.GetTransaction(payload.Data)
+		if err != nil {
+			log.Fatalf("unmarshaling payloadData to transaction error: %s", err)
+		}
+		chaincodeActionPayload, err := unmarshalers.GetChaincodeActionPayload(transaction.Actions[0].Payload)
+		if err != nil {
+			log.Fatalf("unmarshaling transactionActionPayload to chaincodeActionPayload error: %s", err)
+		}
+		proposalResponsePayload, err := unmarshalers.GetProposalResponsePayload(chaincodeActionPayload.Action.ProposalResponsePayload)
+		if err != nil {
+			log.Fatalf("unmarshaling chaincodeActionPayload.Action ProposalResponsePayload to proposalResponsePayload error: %s", err)
+		}
+		chaincodeAction, err := unmarshalers.GetChaincodeAction(proposalResponsePayload.Extension)
+		if err != nil {
+			log.Fatalf("unmarshaling proposalResponsePayload Extension to chaincodeAction error: %s", err)
+		}
+		chaincodeEvent, err := unmarshalers.GetChaincodeEvent(chaincodeAction.Events)
+		if err != nil {
+			log.Fatalf("unmarshaling chaincodeAction.Events to chaincodeEvent error: %s", err)
+		}
+		if chaincodeEvent.EventName != "" {
+			log.Println("############################################################")
+			log.Println("###################### Received event ######################")
+			log.Printf("################### BlockNum : %d ##########################", e.Block.Header.Number)
+			log.Printf("#################### Block event : %v ########### ", chaincodeEvent.EventName)
+			log.Printf("#################### Block info - block %v ########### ", chaincodeAction.String())
+			log.Println("#############################################################")
 		}
 	}
 }
