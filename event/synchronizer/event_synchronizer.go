e87ecf46225193eed0625aeb3bbd85447084ba64
dahuin-1 <dawhite01@naver.com>
2022-06-24 11:24:11 +0900

    edit code

--- event/synchronizer/event_synchronizer.go
+++ event/synchronizer/event_synchronizer.go
@@ -9,15 +9,10 @@ import (
 	"time"
 )
 
-var (
-	startBlockNum uint64
-	endBlockNum   uint64
-	isUpdated     bool
-)
+var startBlockNum uint64
 
 func init() {
 	flag.Uint64Var(&startBlockNum, "startBlock", 0, "set start block number if needed")
-	flag.Uint64Var(&endBlockNum, "endBlock", 0, "set end block number if needed")
 	flag.Parse()
 }
 
@@ -34,98 +29,73 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to get blockchain information, err: %s", err)
 	}
-	currentBlockHeight := blockchainInfo.BCI.Height
-	if startBlockNum == currentBlockHeight {
-		log.Fatal("nothing to sync")
-	}
-	if endBlockNum > currentBlockHeight {
-		log.Println("end block number should be smaller than current block height")
-	}
-	if startBlockNum >= endBlockNum {
-		log.Println("end block number should be bigger than start block number")
-	}
-	if endBlockNum > currentBlockHeight || startBlockNum >= endBlockNum {
-		log.Println("set end block number to current block number")
-		endBlockNum = currentBlockHeight - 1
-		log.Printf("current block number: %d", endBlockNum)
+	if startBlockNum == blockchainInfo.BCI.Height {
+		log.Fatalf("nothing to sync, now block number is %d", blockchainInfo.BCI.Height-1)
 	}
 	if startBlockNum < 1 {
 		log.Println("start block number should be more than 1")
 		log.Println("set start block number to 1")
 		startBlockNum = 1
 	}
-	ticker := time.NewTicker(10 * time.Second)
-	for range ticker.C {
-		synchronize(ledgerClient, currentBlockHeight)
+	ticker := time.NewTicker(time.Second * 10)
+	defer ticker.Stop()
+	for ; true; <-ticker.C {
+		synchronize(ledgerClient)
 	}
-	defer func() {
-		ticker.Stop()
-	}()
 }
 
-func synchronize(ledgerClient *ledger.Client, currentBlockHeight uint64) {
+func synchronize(ledgerClient *ledger.Client) {
 	blockchainInfo, err := ledgerClient.QueryInfo()
 	if err != nil {
 		log.Fatalf("failed to get new blockchain information, err: %s", err)
 	}
-	newBlockHeight := blockchainInfo.BCI.Height
-	if newBlockHeight > currentBlockHeight {
-		startBlockNum = endBlockNum + 1
-		endBlockNum = newBlockHeight - 1
-		isUpdated = false
-	}
-
-	log.Printf("start block number: %d, end block number: %d", startBlockNum, endBlockNum)
-	//i := startBlockNum
-	if !isUpdated {
-		for i := startBlockNum; i <= endBlockNum; i++ {
-			//for i < blockchainInfo.BCI.Height {
-			log.Printf("=================================== Sync on block number: %d ===================================", i)
-			block, err := ledgerClient.QueryBlock(i)
-			if err != nil {
-				log.Fatalf("failed to query Block, err: %s", err)
-			}
-			if block == nil {
-				log.Printf("failed to retrieve the block from blocknumber %d. The block is nil: ", i)
-				panic(err)
-			}
-			blockData := block.Data.Data
-			envelope, err := unmarshalers.GetEnvelopeFromBlock(blockData[0])
-			if err != nil {
-				log.Fatalf("unmarshaling Envelope error: %s", err)
-			}
-			payload, err := unmarshalers.GetPayloadFromEnv(envelope.Payload)
-			if err != nil {
-				log.Fatalf("unmarshaling envelopePayload to payload error: %s", err)
-			}
-			transaction, err := unmarshalers.GetTransaction(payload.Data)
-			if err != nil {
-				log.Fatalf("unmarshaling payloadData to transaction error: %s", err)
-			}
-			chaincodeActionPayload, err := unmarshalers.GetChaincodeActionPayload(transaction.Actions[0].Payload)
-			if err != nil {
-				log.Fatalf("unmarshaling transactionActionPayload to chaincodeActionPayload error: %s", err)
-			}
-			proposalResponsePayload, err := unmarshalers.GetProposalResponsePayload(chaincodeActionPayload.Action.ProposalResponsePayload)
-			if err != nil {
-				log.Fatalf("unmarshaling chaincodeActionPayload.Action ProposalResponsePayload to proposalResponsePayload error: %s", err)
-			}
-			chaincodeAction, err := unmarshalers.GetChaincodeAction(proposalResponsePayload.Extension)
-			if err != nil {
-				log.Fatalf("unmarshaling proposalResponsePayload Extension to chaincodeAction error: %s", err)
-			}
-			chaincodeEvent, err := unmarshalers.GetChaincodeEvent(chaincodeAction.Events)
-			if err != nil {
-				log.Fatalf("unmarshaling chaincodeAction.Events to chaincodeEvent error: %s", err)
-			}
-			if chaincodeEvent.EventName == "" {
-				log.Println("event did not happen")
-			} else {
-				log.Printf("#################### Block event : %v ########### ", chaincodeEvent.EventName)
-				log.Printf("#################### Block info - block %v ########### ", chaincodeAction.String())
-			}
-			i++
+	log.Printf("set start block number: %d waiting for sync...", startBlockNum)
+	i := startBlockNum
+	for i < blockchainInfo.BCI.Height {
+		log.Printf("=================================== Sync on block number: %d ===================================", i)
+		block, err := ledgerClient.QueryBlock(i)
+		if err != nil {
+			log.Fatalf("failed to query Block, err: %s", err)
 		}
-		isUpdated = true
+		if block == nil {
+			log.Fatalf("failed to retrieve the block from blocknumber %d. The block is nil: ", i)
+		}
+		blockData := block.Data.Data
+		envelope, err := unmarshalers.GetEnvelopeFromBlock(blockData[0])
+		if err != nil {
+			log.Fatalf("unmarshaling Envelope error: %s", err)
+		}
+		payload, err := unmarshalers.GetPayloadFromEnv(envelope.Payload)
+		if err != nil {
+			log.Fatalf("unmarshaling envelopePayload to payload error: %s", err)
+		}
+		transaction, err := unmarshalers.GetTransaction(payload.Data)
+		if err != nil {
+			log.Fatalf("unmarshaling payloadData to transaction error: %s", err)
+		}
+		chaincodeActionPayload, err := unmarshalers.GetChaincodeActionPayload(transaction.Actions[0].Payload)
+		if err != nil {
+			log.Fatalf("unmarshaling transactionActionPayload to chaincodeActionPayload error: %s", err)
+		}
+		proposalResponsePayload, err := unmarshalers.GetProposalResponsePayload(chaincodeActionPayload.Action.ProposalResponsePayload)
+		if err != nil {
+			log.Fatalf("unmarshaling chaincodeActionPayload.Action ProposalResponsePayload to proposalResponsePayload error: %s", err)
+		}
+		chaincodeAction, err := unmarshalers.GetChaincodeAction(proposalResponsePayload.Extension)
+		if err != nil {
+			log.Fatalf("unmarshaling proposalResponsePayload Extension to chaincodeAction error: %s", err)
+		}
+		chaincodeEvent, err := unmarshalers.GetChaincodeEvent(chaincodeAction.Events)
+		if err != nil {
+			log.Fatalf("unmarshaling chaincodeAction.Events to chaincodeEvent error: %s", err)
+		}
+		if chaincodeEvent.EventName == "" {
+			log.Println("event did not happen")
+		} else {
+			log.Printf("#################### Block event : %v ########### ", chaincodeEvent.EventName)
+			log.Printf("#################### Block info - block %v ########### ", chaincodeAction.String())
+		}
+		i++
 	}
+	startBlockNum = blockchainInfo.BCI.Height
 }
